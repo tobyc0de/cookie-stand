1b122aaa37ccd24a0eb7b813d3e89b1db5376069
Tobias Foit <tobias.foit@gmail.com>
2023-09-29 15:40:02 +0100

    small change

--- sales.js
+++ sales.js
@@ -104,7 +104,7 @@ LocationFactory.prototype.addTotalRow = function () {
   // create bottom row
   const bottomRow = document.createElement("tr");
   shopTable.appendChild(bottomRow);
-  bottomRow.classList.add("totalrow2");
+  bottomRow.classList.add("totalrow");
   // add Totals cell
   const totalCell2 = document.createElement("td");
   totalCell2.textContent = "Total";
